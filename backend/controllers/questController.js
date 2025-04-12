0725cf8799cf7f45ef8e8af73cedc6194c7da0ff
Serafim <serafimnikolskiy@gmail.com>
2025-04-12 21:23:49 +0300

    some other logic in update Globals variables

--- backend/controllers/questController.js
+++ backend/controllers/questController.js
@@ -33,8 +33,6 @@ const updateQuest = async (req, res) => {
     try {
         const { id } = req.params;
         const { title, description, tags, globalVariables, settings } = req.body;
-        
-        console.log(JSON.stringify(req.body));
         const quest = await Quest.findOneAndUpdate(
             { _id: id, author: req.user._id },
             { title, description, tags, globalVariables, settings },
@@ -46,10 +44,42 @@ const updateQuest = async (req, res) => {
 
         res.status(200).json(quest);
     } catch (error) {
-        console.log('Error while update a quest:' + error.message)
-        res.status(500).json({ message:'Error while update a quest:', error: error.message });
+        console.log('Error while update a quest: ' + error.message)
+        res.status(500).json({ message:'Error while update a quest: ', error: error.message });
     }
 };
+
+const updateQuestVariables = async(req, res) => {
+    try {
+        const { id } = req.params;
+        const { globalVariables } = req.body;
+        
+        const quest = Quest.findOne({ _id: id, author: req.author._id });
+        
+        if (!quest) {
+            return res.status(404).json({ message: 'Quest not found :(' });
+        }
+        
+        if (typeof globalVariables !== 'object') {
+            return res.status(400).json({ message: 'Invalid format' });
+          }
+
+        for (let [key, value] of Object.entries(updates)) {
+            if (value === null) {
+                quest.globalVariables.delete(key);
+            } else {
+                quest.globalVariables.set(key, value);
+            }
+        }
+
+        await quest.save();
+        res.status(200).json({ globalVariables: Object.fromEntries(quest.globalVariables) });
+    }
+    catch (error) {
+        console.log('Error while update quest variables: ', error.message);
+        res.status(500).json({ message: 'Error while update quest variables: ', error: error.message });
+    }
+}
 
 const deleteQuest = async (req, res) => {
     try {
@@ -67,4 +97,4 @@ const deleteQuest = async (req, res) => {
     } 
 };
 
-module.exports = { createQuest, getQuests, updateQuest, deleteQuest };
+module.exports = { createQuest, getQuests, updateQuest, deleteQuest, updateQuestVariables };
--- backend/routes/questRoutes.js
+++ backend/routes/questRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const authMiddleware = require('../middleware/authMiddleware');
-const { createQuest, getQuests, updateQuest, deleteQuest } = require('../controllers/questController');
+const { createQuest, getQuests, updateQuest, deleteQuest, updateQuestVariables } = require('../controllers/questController');
 
 const router = express.Router();
 
@@ -10,5 +10,6 @@ router.post('/', createQuest);
 router.get('/', getQuests);
 router.put('/:id', updateQuest);
 router.delete('/:id', deleteQuest);
+router.patch('/:id/variables', updateQuestVariables);
 
 module.exports = router;
