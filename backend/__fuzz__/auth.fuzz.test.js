8193ddbc6dfeaa53a81e79192d0267e1e2754c7c
aanimelife <serafimnikolskiy@gmail.com>
2025-05-19 21:10:52 +0300

    Dockerfiles

--- backend/__fuzz__/auth.fuzz.test.js
+++ backend/__fuzz__/auth.fuzz.test.js
@@ -8,23 +8,21 @@ describe('Fuzz testing /api/auth/login', () => {
     async () => {
       await fc.assert(
         fc.asyncProperty(
-          fc.string({ maxLength: 100 }), // ограничим длину строки
+          fc.string({ maxLength: 100 }), 
           fc.string({ maxLength: 100 }),
           async (email, password) => {
             const res = await request(app)
               .post('/api/auth/login')
               .send({ email, password });
 
-            // Проверим, что сервер не упал
             expect([200, 400, 401, 500]).toContain(res.statusCode);
           }
         ),
         {
-          numRuns: 30, // ограничим количество попыток
+          numRuns: 30,
           endOnFailure: true,
         }
       );
     },
-    15000 // общий таймаут на тест
   );
 });
