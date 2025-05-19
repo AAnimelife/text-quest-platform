62406fa4cfaf9b4c27ddf04eab65449c1ac3763f
Никольский Серафим <66135336+AAnimelife@users.noreply.github.com>
2025-05-20 00:14:13 +0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -5,15 +5,14 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 
 dotenv.config();
-const cors = require('cors');
+
+const app = express();
 
 app.use(cors({
   origin: ['https://text-quest-platform-main.onrender.com/'],
   credentials: true
 }));
 
-const app = express();
-app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', require('./routes/authRoutes'));
