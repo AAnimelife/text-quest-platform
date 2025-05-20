dc8b95edbd7ef7dab96555eadeda0c7c59d4e350
aanimelife <serafimnikolskiy@gmail.com>
2025-05-20 18:35:41 +0300

    unauthorized reading

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -58,7 +58,7 @@ function App() {
           />
           <Route 
             path="/quest/play/:questId"
-            element={<ProtectedRoute><QuestPlay /></ProtectedRoute>} 
+            element={<QuestPlay />} 
           />
         </Routes>
       </Router>
